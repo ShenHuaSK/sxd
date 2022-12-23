17810995db58bddb3e5c534cbfebd2fc919b1088
droplet-js <8845083+droplet-js@users.noreply.github.com>
2022-12-23 18:01:27 +0800

    Update vite.config.ts

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sxd",
+  base: process.env.PUBLIC_PATH ?? "/", // 部署到 Github Pages 上，故而不能用默认值 "/"
   plugins: [vue()],
   resolve: {
     alias: {
